34bd03d5007a46b8bb3d5051cd84a73b5d451160
LiamWebster25 <wliam2525@gmail.com>
2024-05-20 23:34:54 -0400

    Fixed merge conflict

--- spectra/lib/home.dart
+++ spectra/lib/home.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'models.dart';
 import 'dbhelper.dart';
-<<<<<<< Updated upstream
 import 'perfume_detail.dart';
-import 'add_perfume_screen.dart'; // import add perfume screen
-import 'edit_perfume_screen.dart'; // import edit perfume screen
-=======
 import 'add_perfume_screen.dart';
 import 'edit_perfume_screen.dart';
-import 'perfume_detail.dart';
 import 'search.dart';
->>>>>>> Stashed changes
 
 class MyHomePage extends StatefulWidget {
   final bool isAdmin;
@@ -23,10 +17,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final dbHelper = DatabaseHelper.instance;
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   List<Perfume> perfumes = [];
 
   @override
@@ -63,9 +53,9 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () async {
               final result = await Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => AddPerfumeScreen()),
+                MaterialPageRoute(builder: (context) => AddPerfumeScreen()),
               );
+
               if (result == true) {
                 _queryAll(); // Refresh the list after adding a new perfume
               }
@@ -74,66 +64,25 @@ class _MyHomePageState extends State<MyHomePage> {
         ]
             : [],
       ),
-<<<<<<< Updated upstream
-      body: ListView.separated(
-        itemCount: perfumes.length,
-        separatorBuilder: (context, index) => Divider(color: Colors.grey),
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(10),
-              leading: Image.network(
-                perfumes[index].imageUrl ?? 'https://picsum.photos/250?image=9', // Add your image path here
-                width: 60,
-                height: 60,
-                fit: BoxFit.cover,
-              ),
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    perfumes[index].brand ?? 'Unknown',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    perfumes[index].name ?? 'Name',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  Text(
-                    '${perfumes[index].number}', // Adjust according to your data
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  if (widget.isAdmin)
-=======
       body: ListView(
         shrinkWrap: true,
         children: [
           Container(
             width: 100,
             height: 60,
-            child: ElevatedButton(onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MySearchPage(isAdmin: widget.isAdmin),
-                ),
-              );
-            }, child: Text("Search Page", style: TextStyle(color: Colors.black),),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MySearchPage(isAdmin: widget.isAdmin),
+                  ),
+                );
+              },
+              child: Text(
+                "Search Page",
+                style: TextStyle(color: Colors.black),
+              ),
             ),
           ),
           SizedBox(height: 20),
@@ -142,95 +91,85 @@ class _MyHomePageState extends State<MyHomePage> {
             itemCount: perfumes.length,
             separatorBuilder: (context, index) => Divider(color: Colors.grey),
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${perfumes[index].brand} ${perfumes[index].name}',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text('${perfumes[index].number}'),
-                        ],
+              return Card(
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(10),
+                  leading: Image.network(
+                    perfumes[index].imageUrl ?? 'https://picsum.photos/250?image=9', // Add your image path here
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        perfumes[index].brand ?? 'Unknown',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                    Text(
-                      '\$${perfumes[index].price}',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                onTap: () => _navigateToDetail(perfumes[index]),
-                trailing: widget.isAdmin
-                    ? Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
->>>>>>> Stashed changes
-                    IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () async {
-                        final result = await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-<<<<<<< Updated upstream
-                            builder: (context) => EditPerfumeScreen(perfume: perfumes[index]),
-=======
-                            builder: (context) => EditPerfumeScreen(
-                              perfume: perfumes[index],
-                            ),
->>>>>>> Stashed changes
-                          ),
-                        );
+                      Text(
+                        perfumes[index].name ?? 'Name',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      Text(
+                        '${perfumes[index].number}', // Adjust according to your data
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      if (widget.isAdmin)
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () async {
+                            final result = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EditPerfumeScreen(perfume: perfumes[index]),
+                              ),
+                            );
 
-                        if (result == true) {
-                          _queryAll(); // Refresh the list after updating a perfume
-                        }
-                      },
-                    ),
-<<<<<<< Updated upstream
-                  if (widget.isAdmin)
-=======
->>>>>>> Stashed changes
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        _deletePerfume(perfumes[index].id);
-                      },
-                    ),
-<<<<<<< Updated upstream
-                  Text(
-                    '\$${double.parse(perfumes[index].price ?? '0').toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                            if (result == true) {
+                              _queryAll(); // Refresh the list after updating a perfume
+                            }
+                          },
+                        ),
+                      if (widget.isAdmin)
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            _deletePerfume(perfumes[index].id);
+                          },
+                        ),
+                      Text(
+                        '\$${double.parse(perfumes[index].price ?? '0').toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PerfumeDetailScreen(perfume: perfumes[index]),
-                  ),
-                );
-              },
-            ),
-          );
-        },
-=======
-                  ],
-                )
-                    : null,
+                  onTap: () {
+                    _navigateToDetail(perfumes[index]);
+                  },
+                ),
               );
             },
           ),
         ],
->>>>>>> Stashed changes
       ),
     );
   }
